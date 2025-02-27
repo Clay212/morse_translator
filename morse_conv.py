c7a202af7d881edeac8d607eec29cb6f2e26960b
Clay212 <djohnlaei@gmail.com>
2025-02-27 21:37:55 +0800

    while loop

--- morse_conv.py
+++ morse_conv.py
@@ -14,29 +14,35 @@ morse_code = {
     ";": "-*-*-*", "=": "-***-", "+": "*-*-*", "-": "-****-", "_": "**--*-",
     '"': "*-**-*", "$": "***-**-", "@": "*--*-*"
 }
-print("-Morse Code Translator-")
-print("Option1: Translate English to Morse code")
-print("Option2: Translate Morse Code to English")
 
-option = input("\nEnter an option (1 or 2): ")
+while True:
+    print("-Morse Code Translator-")
+    print("Option1: Translate English to Morse code")
+    print("Option2: Translate Morse Code to English")
 
-#Option 1: Translate English to Morse Code 
-if option == "1":
-    text = input("Enter a text: ").strip()
-#converting each char to morse code in library
-    morse = "".join(morse_code.get(char.upper(), "?")for char in text)
-    print("Morse Code:", morse)  
+    option = input("\nEnter an option (1 or 2): ")
 
-#Option 2: Translate Morse Code to English
-elif option == "2":
-    morse = input("Enter Morse code: ").strip()
-    text = ""
+    #Option 1: Translate English to Morse Code 
+    if option == "1":
+        text = input("Enter a text: ").strip()
+    #converting each char to morse code in library
+        morse = "".join(morse_code.get(char.upper(), "?")for char in text)
+        print("Morse Code:", morse)  
 
-    morse_dict = {v: k for k, v in morse_code.items()}  # Reverse dictionary
-        text = "".join(morse_dict.get(part, "?") for part in morse.split(" "))
+    #Option 2: Translate Morse Code to English
+    elif option == "2":
+        morse = input("Enter Morse code: ").strip()
+        text = ""
+    else:
+            print("Invalid option! Try again.")
+            continue 
+
+        morse_dict = {v: k for k, v in morse_code.items()}  # Reverse dictionary
+            text = "".join(morse_dict.get(part, "?") for part in morse.split(" "))
         
-        print("English Text:", text)
-#Ask for another trial
- again = input("\nDo you want to try again? (y/n): ").strip().lower()
-    if again != 'y':
-        print("Exiting the Morse code translator. Goodbye!")
+            print("English Text:", text)
+    #Ask for another trial
+     again = input("\nDo you want to try again? (y/n): ").strip().lower()
+        if again != 'y':
+            print("Exiting the Morse code translator. Goodbye!")
+            break
